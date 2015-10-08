c6653e8274bdae0d5fd3f45c0c325e81511aeb99
Mark Brown II <markbrown09@gmail.com>
2015-10-08 00:27:47 -0400

    Perfected the Player movement with the water and boundaries

--- js/app.js
+++ js/app.js
@@ -28,8 +28,8 @@ Enemy.prototype.render = function() {
 
 var Player = function() {
     this.sprite = 'images/char-princess-girl.png';
-    this.x = 150;
-    this.y = 200;
+    this.x = 200;
+    this.y = 375;
     //this.checkCollison() = function() {}; //reset the game when player and bug collide
 };
 // This class requires an update(), render() and
@@ -39,33 +39,33 @@ Player.prototype.render = function() {
 };
 
 Player.prototype.update = function(dt) {
-    if (this.x > 500) {
-        this.x = 50;
+    if (this.x > 450) {
+        this.x = 400;
     }
     else if (this.x < 0) {
-        this.x = 30;
+        this.x = 0;
     }
     else if (this.y > 430) {
-        this.y = 420;
+        this.y = 375;
     }
     else if (this.y < 50) {
-        this.y = 300;
-        this.x = 100;
+        this.y = 375;
+        this.x = 200;
     }
 };
 
 Player.prototype.handleInput = function(key) {
    if (key === 'left') {
-            this.x = this.x - 30; //this needs a limitation
+            this.x = this.x - 100; //this needs a limitation
     }
     else if (key === 'right') {
-            this.x = this.x + 30; ///this needs a limitation
+            this.x = this.x + 100; ///this needs a limitation
     }
     else if (key === 'up') {
-            this.y = this.y - 30;
+            this.y = this.y - 80;
     }
     else  if (key === 'down') {
-            this.y = this.y + 30;
+            this.y = this.y + 80;
     }
 };
 
